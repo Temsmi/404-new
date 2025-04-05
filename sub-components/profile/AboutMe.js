133b6450ee92a2d8b9be07c929ab30e1a02c48e7
somayehahmadii <ahmadinejadsomayeh@gmail.com>
2025-04-05 23:05:41 +0300

    setting profile connect to database

--- sub-components/profile/AboutMe.js
+++ sub-components/profile/AboutMe.js
@@ -1,44 +1,56 @@
-// import node module libraries
-import { Col, Row, Card } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { Col, Row, Card, Spinner } from 'react-bootstrap';
 
 const AboutMe = () => {
-    return (
-        <Col xl={6} lg={12} md={12} xs={12} className="mb-6">
-            {/* card */}
-            <Card>
-                {/* card body */}
-                <Card.Body>
-                    {/* card title */}
-                    <Card.Title as="h4">About Me</Card.Title>
-                    <span className="text-uppercase fw-medium text-dark fs-5 ls-2">Bio</span>
-                    <p className="mt-2 mb-6"> I was born to become a club president!
-                    </p>
-                    <Row>
-                        <Col xs={12} className="mb-5">
-                            <h6 className="text-uppercase fs-5 ls-2">Department</h6>
-                            <p className="mb-0">Sinema</p>
-                        </Col>
-                        <Col xs={6} className="mb-5">
-                            <h6 className="text-uppercase fs-5 ls-2">Phone </h6>
-                            <p className="mb-0">+53344761325</p>
-                        </Col>
-                        <Col xs={6} className="mb-5">
-                            <h6 className="text-uppercase fs-5 ls-2">Date of Birth </h6>
-                            <p className="mb-0">07.08.2003</p>
-                        </Col>
-                        <Col xs={6}>
-                            <h6 className="text-uppercase fs-5 ls-2">Email </h6>
-                            <p className="mb-0">President@gmail.com</p>
-                        </Col>
-                        <Col xs={6}>
-                            {/* <h6 className="text-uppercase fs-5 ls-2">Location</h6>
-                            <p className="mb-0">Ahmedabad, India</p> */}
-                        </Col>
-                    </Row>
-                </Card.Body>
-            </Card>
-        </Col>
-    )
-}
+  const [president, setPresident] = useState(null);
 
-export default AboutMe
+  useEffect(() => {
+    const fetchPresident = async () => {
+      try {
+        const res = await fetch('/api/setting');
+        const data = await res.json();
+        setPresident(data);
+      } catch (error) {
+        console.error('Error fetching president:', error);
+      }
+    };
+    fetchPresident();
+  }, []);
+
+  if (!president) {
+    return (
+      <div className="text-center mt-4">
+        <Spinner animation="border" variant="primary" />
+        <p>Loading president's information...</p>
+      </div>
+    );
+  }
+
+  return (
+    <Col xl={6} lg={12} md={12} xs={12} className="mb-6">
+      <Card>
+        <Card.Body>
+          <Card.Title as="h4">About {president.name} {president.surname}</Card.Title>
+          <span className="text-uppercase fw-medium text-dark fs-5 ls-2">Bio</span>
+          <p className="mt-2 mb-6">I was born to become a club president!</p>
+          <Row>
+            <Col xs={12} className="mb-5">
+              <h6 className="text-uppercase fs-5 ls-2">Department</h6>
+              <p className="mb-0">{president.dept}</p>
+            </Col>
+            <Col xs={6} className="mb-5">
+              <h6 className="text-uppercase fs-5 ls-2">Phone</h6>
+              <p className="mb-0">{president.phone_num}</p>
+            </Col>
+            <Col xs={6}>
+              <h6 className="text-uppercase fs-5 ls-2">Email</h6>
+              <p className="mb-0">{president.email}</p>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+export default AboutMe;
