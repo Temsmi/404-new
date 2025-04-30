51fe649132d1d03d51a11ff3cb14bb55300cbd1d
Temsmi <temitopeocheje@gmail.com>
2025-04-30 13:16:22 +0300

    Revert "Render Test 2"
    This reverts commit 8b417d9fa570f32ed72533cd0afad2d367e83efb.

---
+++ app/api/member/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+import { conn } from '../../connections/conn';
+
+export async function GET() {
+    try {
+        const query = `
+            SELECT 
+                members.id,
+                members.date_joined,
+                student.name AS std_name, 
+                student.surname AS std_sname,
+                student.std_no AS std_no,
+                student.dept AS std_dept
+            FROM members
+            LEFT JOIN student ON members.student_id = student.id
+            LEFT JOIN club ON members.club_id = club.id
+            GROUP BY members.date_joined, student.name
+        `;
+
+        const members = await conn({ query });
+
+        return NextResponse.json(members);
+    } catch (error) {
+        console.error(" Database error:", error);
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
